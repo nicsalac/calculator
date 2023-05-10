a11f26af2948517ad0a166554bae730ed36077a5
nicsalac <nicoletteirishsalac127@gmail.com>
2023-05-10 18:38:12 +0800

    def multiply

--- calculator.py
+++ calculator.py
@@ -5,3 +5,7 @@ def add(number_one, number_two):
 # This function subtracts two numbers
 def subtract(number_one, number_two):
     return number_one - number_two
+
+# This function multiplies two numbers
+def multiply(number_one, number_two):
+    return number_one * number_two
