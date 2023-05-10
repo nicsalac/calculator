33836b994d86ac474117c4c4724471ee2298968c
nicsalac <nicoletteirishsalac127@gmail.com>
2023-05-10 18:49:01 +0800

    While looping

--- calculator.py
+++ calculator.py
@@ -14,3 +14,18 @@ def multiply(number_one, number_two):
 def divide(number_one, number_two):
     return number_one / number_two
 
+while True:
+    # take the input from the user
+    choice = input("Enter choice(1/2/3/4): ")
+
+    # check if choice is one of the four options
+    if choice in ('1', '2', '3', '4'):
+        try:
+            num1 = float(input("Enter first number: "))
+            num2 = float(input("Enter second number: "))
+        except ValueError:
+            print("Invalid input. Please enter a number.")
+            continue
+
+
+
