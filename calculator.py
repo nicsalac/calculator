36eae81cfe0738905143d1c05a414b52a285d3e1
nicsalac <nicoletteirishsalac127@gmail.com>
2023-05-10 18:41:43 +0800

    def divide

--- calculator.py
+++ calculator.py
@@ -9,3 +9,8 @@ def subtract(number_one, number_two):
 # This function multiplies two numbers
 def multiply(number_one, number_two):
     return number_one * number_two
+
+# This function divides two numbers
+def divide(number_one, number_two):
+    return number_one / number_two
+
